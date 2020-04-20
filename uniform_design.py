cda19d7894e7a62be5cb115c5dd5252f777449cb
Fong <karta379583@gmail.com>
2020-04-21 03:16:43 +0800

    bug fixing

--- uniform_design.py
+++ uniform_design.py
@@ -5,74 +5,107 @@ Created on Sun Dec 22 18:06:44 2019
 @author: CILAB
 """
 #import pandas as pd
-import numpy as np
 import math
-from numpy import genfromtxt
-
 from glob import glob
 from os.path import join
 
-def design(table):
-    list_columns = [int(x)-1 for x in input('column:').split()]
-    print(list_columns, '\n')
-    union_table = table[:,list_columns]
-    return union_table
+import numpy as np
+from numpy import genfromtxt
+
+#def design(table):
+#    list_columns = [int(x)-1 for x in input('column:').split()]
+#    print(list_columns, '\n')
+#    uniform_table = table[:, list_columns]
+#    return uniform_table
+
+def udt_combination():
+    """ different combinations of UDT columns """
+    global N, S
     
-def table(n, s):
-    de_table = np.ndarray((n, s), dtype=np.intp)
-    uni_cols = np.ndarray((n, s), dtype=np.intp) #record columns of UDT that after using modulo 
-    for i in range(n):
-        for j in range(s):
-            de_table[i, j] = (i+1) * (j+1)
-            uni_cols[i, j] = de_table[i, j] % n
-            if uni_cols[i, j] == 0:
-                uni_cols[i, j] = n
-                
+    rows = int(math.factorial(N)/ (math.factorial(S) * math.factorial(N-S)))
+    columns = np.zeros((rows, S), dtype=np.intp)
+    col_list = list()
+
+    for i in range(S):
+        col_list.append(i)
+
+    col_count = 0
+    while col_list[S-1] < N: #while the last element smaller than N, meaning end of the loop
+        columns[col_count] = col_list
+        col_count += 1
+        print(col_list)
+
+        col_list[-1] += 1
+
+        if col_list[S-1] == N:
+            for i in reversed(range(S-1)):
+                if col_list[i]+1 <= N-S+i:
+                    col_list[i] += 1
+                    while i < (S-1):
+                        col_list[i+1] = col_list[i]+1
+                        i += 1
+                    break;
+    return columns
     
-    with np.printoptions():
-        print("U_{}_{}:".format(n, s))
-        print(de_table)
-        
-    with np.printoptions():
-        print("\nU_{}_{} after using modulo:".format(n, s))
-        print(uni_cols)
-    
-    # generate maximum avaible columns of UDT
+
+def uniform_design_table(modulo_table):
+    """ generate default and modulo table """
+    global N
     col = list() #record avaible columns
-    for i in range(n): #compute the greatest common divisor of every column and N(experiment number)
-        if math.gcd(i, n) == 1:
+
+    for i in range(N): #compute greatest common divisor of every column and N(experiment number)
+        if math.gcd(i, N) == 1:
             col.append(i)
-    
+
     for i in col:
         if i == 1:
-            uni_table = np.array([uni_cols[:,0]])
+            uni_table = np.array([modulo_table[:, 0]])
         else:
-            uni_table = np.concatenate((uni_table, np.array([uni_cols[:,i-1]])), axis=0)
-            
+            uni_table = np.concatenate((uni_table, np.array([modulo_table[:, i-1]])), axis=0)
+
     uni_table = uni_table.T
-    
+
     with np.printoptions():
-        print("\nU_{}_{}:".format(n, len(col)))
+        print("\nU({},{}):".format(N, len(col)))
         print(uni_table)
-        
+
     return uni_table
 
-def is_default():
-#    n = int(input('n = '))
-    n = 12
-    s = n-1
-    return n, s        
+def table():
+    """ generate default and modulo table """
+    global N
+    default_table = np.zeros((N, N-1), dtype=np.intp) #default uniform design table
+    modulo_table = np.zeros((N, N-1), dtype=np.intp) #record columns of UDT that after modulo operation
+    for i in range(N):
+        for j in range(N-1):
+            default_table[i, j] = (i+1) * (j+1)
+            modulo_table[i, j] = default_table[i, j] % N #modulo operation
+            if modulo_table[i, j] == 0:
+                modulo_table[i, j] = N
+
+    with np.printoptions():
+        print("U({},{}):".format(N, N-1))
+        print(default_table)
+
+    with np.printoptions():
+        print("\nU({},{}) after using modulo:".format(N, N-1))
+        print(modulo_table)
+
+    return default_table, modulo_table
 
 if __name__ == '__main__':
-    N, S = is_default()
-    table = table(N, S)
-#    union_table = design(table)
-#    print("uniform design table:\n",union_table, '\n')
-    
-#    scheme = np.ndarray((union_table.shape[0], union_table.shape[1]), dtype='f')
+    N = 9
+    S = 5
+    DEFAULT_TABLE, MODULO_TABLE = table()
+    UDT = uniform_design_table(MODULO_TABLE)
+    UDT_COMBINATION = udt_combination()
+
+#    uniform_table = design(table)
+#    print("uniform design table:\n",uniform_table, '\n')
+
+#    scheme = np.ndarray((uniform_table.shape[0], uniform_table.shape[1]), dtype='f')
 #    for file in glob(join('*.csv')):
 #        csv = genfromtxt(file, delimiter = ',', skip_header = 1)
 #        for i in range(csv.shape[0]):
 #            for j in range(csv.shape[1]):
-#                scheme [i, j] = csv[union_table[i, j]-1, j]
-    
+#                scheme [i, j] = csv[uniform_table[i, j]-1, j]
